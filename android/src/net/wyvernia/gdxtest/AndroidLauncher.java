86b6f3993b2f305b2e8bfbc1df119dd35a01573d
João Braz <joaofgbraz@gmail.com>
2016-04-15 15:42:16 +0100

    Initial commit.

---
+++ android/src/net/wyvernia/gdxtest/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package net.wyvernia.gdxtest;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import net.wyvernia.gdxtest.MyGdxTest;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxTest(), config);
+	}
+}
