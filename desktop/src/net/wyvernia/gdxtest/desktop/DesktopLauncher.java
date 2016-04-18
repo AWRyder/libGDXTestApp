afd0f4da2e7c51b86cf854af247749f0c64650ad
João Braz <joaofgbraz@gmail.com>
2016-04-18 19:20:43 +0100

    Added screens from the extended tutorial. Removed old code.

--- desktop/src/net/wyvernia/gdxtest/desktop/DesktopLauncher.java
+++ desktop/src/net/wyvernia/gdxtest/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package net.wyvernia.gdxtest.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import net.wyvernia.gdxtest.MyGdxTest;
+import net.wyvernia.gdxtest.Drop;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGdxTest(), config);
+		new LwjglApplication(new Drop(), config);
 	}
 }
